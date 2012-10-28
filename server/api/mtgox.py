5bfa4c4a7f46d00e347f91b045d3c057c0876ae8
Greg Balaga <greg.balaga@gmail.com>
2012-10-28 19:31:25 +0100

    fix the convert function

--- server/api/mtgox.py
+++ server/api/mtgox.py
@@ -9,10 +9,10 @@ from urllib import urlencode
 
 class mtgox:
 	# Settings
-	API_KEY	= '' #### API KEY
-	SECRET	= b64decode('') #### ENCODED SECRET
-	LOGIN	= '' #### LOGIN
-	PASS	= '' #### PASS
+	API_KEY	= '360e28ab-b5a8-40fa-e11c-25d271947c51'
+	SECRET	= b64decode('JNUmav6DNbBLgPJLfVCRmNhDGbpzIKbhIe2ZgxAt0RsJNy45wNLModKURZ3j5O9C7XlxjLbk8XkFbPrydeQnJQ==')
+	LOGIN	= 'Dreen'
+	PASS	= 'a&_K4+j%O-}<4BQ'
 	VERSION	= '1'
 	
 	# Locations
@@ -55,14 +55,14 @@ class mtgox:
 			r.get.update({'raw': 1})
 		if self.toUTF:
 			return self.convert(json.loads(r.go()))
-		else
+		else:
 			return json.loads(r.go())
 	
 	def convert(self, input):
 		if isinstance(input, dict):
-			return {convert(key): convert(value) for key, value in input.iteritems()}
+			return dict((self.convert(key), self.convert(value)) for key, value in input.iteritems())
 		elif isinstance(input, list):
-			return [convert(element) for element in input]
+			return [self.convert(element) for element in input]
 		elif isinstance(input, unicode):
 			return input.encode('utf-8')
 		else:
