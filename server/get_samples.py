7a27872e00e05a4ad0a236a580fbd8b233564dcf
Greg Balaga <greg.balaga@gmail.com>
2012-11-01 13:48:41 +0100

    finished get-samples
    making sure files generated by the script are readable by jq

--- server/get_samples.py
+++ server/get_samples.py
@@ -1,13 +1,17 @@
 from api import mtgox, intersango
 from pprint import pformat
+import re
 
 m = mtgox.mtgox()
 i = intersango.intersango()
+pt_escape = re.compile(r'\\x[0-9a-f]{2}')
 
 def save(result, file):
-    f = open(file, 'w')
-    f.write(pformat(result).replace("'",'"'))
-    f.close()
+	f = open(file, 'w')
+	resultStr = pformat(result).replace("'",'"').replace(': None',': null')
+	resultStr = pt_escape.sub('', resultStr)
+	f.write(resultStr)
+	f.close()
 
 save(m.currency(7,'ticker'),			'samples/mtgoxGBPticker.json')
 save(m.currency(7,'depth'),				'samples/mtgoxGBPdepth.json')
