ae6a48b4463d7025745280399a10d9a81651385b
Dreen <dreen1@gmail.com>
2013-04-28 17:21:36 +0100

    simple test for API

--- server/test/test.api.js
+++ server/test/test.api.js
@@ -1,1 +1,17 @@
- 
+var assert = require('assert'),
+	API = require('../api/api.js');
+
+var data_address = {
+	'host': 'raw.github.com',
+	'path': '/Dreen/BitcoinAPIbrowser/master/server/test/data/'
+}
+
+exports['simple GET retrieval and parsing'] = function(done)
+{
+	var api = new API(true);
+	var request = data_address;
+	request['path'] += 'simple.json';
+	var result = api.go(request);
+	assert.ok(result['result'] == 'OK');
+	done();
+};
