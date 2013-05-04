12e06cfa3a514947fa45d7bc5f91404ae997cf4d
Dreen <dreen1@gmail.com>
2013-05-04 18:46:26 +0100

    incorporate a test for a simple get retrieval

--- server/test/test.api.js
+++ server/test/test.api.js
@@ -40,6 +40,28 @@ function testSuite(APIOBJ, objname)
 				assert.strictEqual(api.verbose, true);
 			});
 		});
+
+		describe('GET retrieval', function()
+		{
+			var api = new APIOBJ();
+			var result = {};
+			api.setCallback(function(data)
+			{
+				result = data;
+			});
+
+			it('simple', function(done)
+			{
+				var request = api.go({
+					'host': 'ec2-54-245-170-7.us-west-2.compute.amazonaws.com',
+					'path': '/~ec2-user/data/simple.json',
+					'port': 80
+				});
+				api.go(request);
+				assert.ok(result['result'] == 'OK');
+				done();
+			});
+		});
 	};
 }
 
@@ -55,20 +77,4 @@ function TestAPI()
 util.inherits(TestAPI, API);
 describe('child', testSuite(TestAPI, "TestAPI"));
 
-/*
-	describe('simple GET retrieval and parsing', function()
-	{
-		it('should parse OK', function(done)
-		{
-			var result = api.go({
-				'host': 'ec2-54-245-170-7.us-west-2.compute.amazonaws.com',
-				'path': '/~ec2-user/data/simple.json',
-				'port': 80
-			});
-			api.go(request);
-			assert.ok(result['result'] == 'OK');
-			done();
-		});
-	});
-*/
 
