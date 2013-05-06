d66e1e3490f157a9936f998b349f21ab957613d7
Dreen <dreen1@gmail.com>
2013-05-07 00:26:09 +0100

    define server model and load it during server start up

--- server/main.js
+++ server/main.js
@@ -1,48 +1,62 @@
-var io = require('socket.io');
-
+//var io = require('socket.io');
+var mongo 	= require('mongodb'),
+fs		= require('fs');
 //var misc	= require('./misc.js');
 
 // run as main module only
 if (!module.parent)
 {
-	var port = process.argv[2] || 8000;
+	//var port = process.argv[2] || 8000;
 	
-	// TODO connect to mongo?
-	
-	// TODO read the server model
-	
-	// TODO start the request bot
-	
-	// TODO logging
-	
-	// cPool?
-	
-	// shutdown handler
-	var shutdown = function()
+	// connect to mongo
+	mongo.MongoClient.connect("mongodb://localhost:27017", function(err, db)
 	{
-		onsole.log('Server shutting down');
-		process.exit();
-	}
-	process.on('SIGTERM', shutdown);
-	process.on('SIGQUIT', shutdown);
-	process.on('SIGINT', shutdown);
-	
-	// start the server
-	console.log('Serving at port %d', port);
-	var server = io.listen(port);
-	
-	// server handler
-	server.sockets.on('connection' , function(socket)
-	{
-		console.log('connected id:%s', socket.id);
-		socket.on('message', function(msg) // TODO optional arg callback useful?
+		if (err) throw err;
+
+		// read the server model from a file and update it in the db
+		var model = db.collection('model');
+		model.remove();
+		var serverModel = JSON.parse(fs.readFileSync(__dirname + '/serverModel.json').toString());
+		for (var i=0; i<serverModel.length; i++)
 		{
-			socket.send(msg);
-		});
+			model.insert(serverModel[i], function(err, result) { if (err) throw err; });
+		}
 		
-		socket.on('disconnect', function()
-		{
-			console.log('disconnected id:%s', socket.id);
-		});
+		console.log("done");
+		
+		// TODO start the request bot
+		
+		// TODO logging
+		
+		// cPool?
+		
+		// shutdown handler
+		//var shutdown = function()
+		//{
+		//	onsole.log('Server shutting down');
+		//	process.exit();
+		//}
+		//process.on('SIGTERM', shutdown);
+		//process.on('SIGQUIT', shutdown);
+		//process.on('SIGINT', shutdown);
+		//
+		//// start the server
+		//console.log('Serving at port %d', port);
+		//var server = io.listen(port);
+		//
+		//// server handler
+		//server.sockets.on('connection' , function(socket)
+		//{
+		//	console.log('connected id:%s', socket.id);
+		//	socket.on('message', function(msg) // TODO optional arg callback useful?
+		//	{
+		//		socket.send(msg);
+		//	});
+		//	
+		//	socket.on('disconnect', function()
+		//	{
+		//		console.log('disconnected id:%s', socket.id);
+		//	});
+		//});
 	});
 }
