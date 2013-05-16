1fc8f2e82c99afdf4528914a417dfb554f9ccf74
Dreen <dreen1@gmail.com>
2013-05-16 23:30:13 +0100

    up to date server should do basic setup + echo

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
-//var io = require('socket.io');
 var
 mongo	= require('mongodb'),
+io	= require('socket.io')
 Winston = require('winston'),
+quitter	= require('shutdown-handler'),
 fs	= require('fs'),
 
 Bot	= require('./bot.js')(),
@@ -12,49 +13,47 @@ logger	= new Winston.Logger({transports: [new Winston.transports.Console()]});
 if (!module.parent)
 {
 	logger.info('Initiating');
-	//var port = process.argv[2] || 8000;
+	var port = process.argv[2] || 8000;
 	
 	// connect to mongo
 	mongo.MongoClient.connect("mongodb://localhost:27017/bitapi", function(err, db)
 	{
-		if (err) throw err;
-
 		// read the server model from a file and update it in the db
-		//var model = db.collection('model');
-		//model.remove({w:0});
-		//var serverModel = JSON.parse(fs.readFileSync(__dirname + '/serverModel.json').toString());
-		//for (var i=0; i<serverModel.length; i++)
-		//{
-		//	model.insert(serverModel[i], {w:0});
-		//}
+		var model = db.collection('model');
+		model.remove({w:0});
+		var serverModel = JSON.parse(fs.readFileSync(__dirname + '/serverModel.json').toString());
+		for (var i=0; i<serverModel.length; i++)
+		{
+			model.insert(serverModel[i], {w:0});
+		}
 		
 		// start the request bot
-		//var bot = new Bot(db);
-		//bot.start();
+		var bot = new Bot(db);
+		bot.start();
 		
 		// shutdown handler
-		require('shutdown-handler').on('exit', function() {
+		quitter.on('exit', function() {
 			logger.info('Server shutting down');
 		});
 
 		// TODO server
-		//// start the server
-		//console.log('Serving at port %d', port);
-		//var server = io.listen(port);
-		//
-		//// server handler
-		//server.sockets.on('connection' , function(socket)
-		//{
-		//	console.log('connected id:%s', socket.id);
-		//	socket.on('message', function(msg) // TODO optional arg callback useful?
-		//	{
-		//		socket.send(msg);
-		//	});
-		//	
-		//	socket.on('disconnect', function()
-		//	{
-		//		console.log('disconnected id:%s', socket.id);
-		//	});
-		//});
+		// start the server
+		console.log('Serving at port %d', port);
+		var server = io.listen(port);
+		
+		// server handler
+		server.sockets.on('connection' , function(socket)
+		{
+			console.log('connected id:%s', socket.id);
+			socket.on('message', function(msg) // TODO optional arg callback useful?
+			{
+				socket.send(msg);
+			});
+			
+			socket.on('disconnect', function()
+			{
+				console.log('disconnected id:%s', socket.id);
+			});
+		});
 	});
 }
