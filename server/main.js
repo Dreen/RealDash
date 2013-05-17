cfb008a12d24f9ec009a7cb3a10c250e0250830a
Dreen <dreen1@gmail.com>
2013-05-17 10:01:47 +0100

    correct startup

--- server/main.js
+++ server/main.js
@@ -1,13 +1,15 @@
 var
 mongo	= require('mongodb'),
-io	= require('socket.io')
+io	= require('socket.io'),
 Winston = require('winston'),
 quitter	= require('shutdown-handler'),
 fs	= require('fs'),
 
 Bot	= require('./bot.js')(),
 
-logger	= new Winston.Logger({transports: [new Winston.transports.Console()]});
+logger	= new Winston.Logger({transports: [new Winston.transports.Console()]}),
+
+err_handler = function(){}; //TODO
 
 // run as main module only
 if (!module.parent)
@@ -19,24 +21,22 @@ if (!module.parent)
 	mongo.MongoClient.connect("mongodb://localhost:27017/bitapi", function(err, db)
 	{
 		// read the server model from a file and update it in the db
-		var model = db.collection('model');
-		model.remove({w:0});
-		var serverModel = JSON.parse(fs.readFileSync(__dirname + '/serverModel.json').toString());
-		for (var i=0; i<serverModel.length; i++)
+		var model = db.collection('model'),
+		bot;
+		model.remove(function()
 		{
-			model.insert(serverModel[i], {w:0});
-		}
-		
-		// start the request bot
-		var bot = new Bot(db);
-		bot.start();
-		
-		// shutdown handler
-		quitter.on('exit', function() {
-			logger.info('Server shutting down');
+			var serverModel = JSON.parse(fs.readFileSync(__dirname + '/serverModel.json').toString());
+			for (var i=0; i<serverModel.length; i++)
+			{
+				model.insert(serverModel[i], (i < serverModel.length - 1) ? err_handler : function()
+				{
+					// start the request bot
+					bot = new Bot(db);
+					bot.start();
+				});
+			}
 		});
 
-		// TODO server
 		// start the server
 		console.log('Serving at port %d', port);
 		var server = io.listen(port);
@@ -54,6 +54,12 @@ if (!module.parent)
 			{
 				console.log('disconnected id:%s', socket.id);
 			});
+		});
+		
+		// shutdown handler
+		quitter.on('exit', function() {
+			bot.shutdown();
+			logger.info('Server shutting down');
 		});
 	});
 }
