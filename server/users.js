89f1ffc1f75997cf7ff1fddf2d355f170f415e5b
Dreen <dreen1@gmail.com>
2013-05-26 23:26:57 +0100

    users.js inits users

--- server/users.js
+++ server/users.js
@@ -1,21 +1,24 @@
+var User = require('./user.js')();
+
 functions Users()
 {
-	this._clients = {};
+	this._users = {};
 }
 
 Users.prototype.get = function(id)
 {
-	return this._clients[id];
+	return this._users[id];
 }
 
 Users.prototype.add = function(client)
 {
-	this._clients[client.id] = client;
+	this._users[client.id] = new User(client);
 }
 
 Users.prototype.remove = function(id)
 {
-	del this._clients[id];
+	// ? this._users[id].remove();
+	del this._users[id];
 }
 
 module.exports = new Users();
