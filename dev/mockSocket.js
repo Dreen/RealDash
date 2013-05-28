eb0dd21faf17083c4f6d06465d026929505eba1e
Dreen <dreen1@gmail.com>
2013-05-28 16:12:54 +0100

    message handling tests for user, mock socket not actually useful

---
+++ dev/mockSocket.js
@@ -0,0 +1,38 @@
+function MockSocket()
+{
+	EE.call(this);
+	this.id = "-dHkm7vtrjbWZewW6m_O";
+	this.handshake = { 
+		headers: {
+			'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6',
+			origin: 'null',
+			'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrom... (length: 108)',
+			host: '127.0.0.1:8000',
+			dnt: '1',
+			'accept-encoding': 'gzip,deflate,sdch',
+			'cache-control': 'max-age=0',
+			connection: 'keep-alive',
+			accept: '*/*'
+		},
+		address: { address: '127.0.0.1', port: 53042 },
+		url: '/socket.io/1/?t=1369690849393',
+		xdomain: true,
+		secure: undefined,
+		time: 'Mon May 27 2013 22:40:49 GMT+0100 (GMT Daylight Time)',
+		query: { t: '1369690849393' }
+	};
+	this.flags = { endpoint: '', room: '' };
+
+	this.on('close', function(){console.log('socket:close')});
+	this.on('message', function(){console.log('socket:message')});
+	this.on('error', function(){console.log('socket:error')});
+	this.on('flush', function(){console.log('socket:flush')});
+	this.on('drain', function(){console.log('socket:drain')});
+	this.on('packet', function(){console.log('socket:packet')});
+	this.on('packetCreate', function(){console.log('socket:packetCreate')});
+}
+
+util.inherits(mockSocket, EE);
+
+MockSocket.prototype.send = function(msg){console.log('socket.send ' + msg)};
+MockSocket.prototype.close = function(){console.log('socket.close')};
--- server/test/test.user.js
+++ server/test/test.user.js
@@ -1,51 +1,11 @@
 var
 assert	= require('assert'),
 User	= require('../user.js')(true),
-EE	= require('events').EventEmitter,
 users	= require('../users.js');
-
-function MockSocket()
-{
-	EE.call(this);
-	this.id = "-dHkm7vtrjbWZewW6m_O";
-	this.handshake = { 
-		headers: {
-			'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6',
-			origin: 'null',
-			'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrom... (length: 108)',
-			host: '127.0.0.1:8000',
-			dnt: '1',
-			'accept-encoding': 'gzip,deflate,sdch',
-			'cache-control': 'max-age=0',
-			connection: 'keep-alive',
-			accept: '*/*'
-		},
-		address: { address: '127.0.0.1', port: 53042 },
-		url: '/socket.io/1/?t=1369690849393',
-		xdomain: true,
-		secure: undefined,
-		time: 'Mon May 27 2013 22:40:49 GMT+0100 (GMT Daylight Time)',
-		query: { t: '1369690849393' }
-	};
-	this.flags = { endpoint: '', room: '' };
-
-	this.on('close', function(){console.log('socket:close')});
-	this.on('message', function(){console.log('socket:message')});
-	this.on('error', function(){console.log('socket:error')});
-	this.on('flush', function(){console.log('socket:flush')});
-	this.on('drain', function(){console.log('socket:drain')});
-	this.on('packet', function(){console.log('socket:packet')});
-	this.on('packetCreate', function(){console.log('socket:packetCreate')});
-}
-
-util.inherits(mockSocket, EE);
-
-MockSocket.prototype.send = function(msg){console.log('socket.send ' + msg)};
-MockSocket.prototype.close = function(){console.log('socket.close')};
 
 describe('User', function()
 {
-	var user = users.add(new MockSocket());
+	var user = users.add("-dHkm7vtrjbWZewW6m_O");
 
 	it('added to pool (lookup)', function()
 	{
@@ -63,6 +23,54 @@ describe('User', function()
 		{
 			assert.ok(msg.isValid);
 			assert.equal(msg.getCmd());
+			done();
+		});
+		user.inbox(msg);
+	});
+
+	it('send a valid message', function(done)
+	{
+		var msg = {
+			cid: "-dHkm7vtrjbWZewW6m_O",
+			cmd: "test",
+			args: [1, 2, 3]
+		};
+		user.on('send', function(msg)
+		{
+			assert.ok(msg.isValid);
+			assert.equal(msg.getCmd());
+			done();
+		});
+		user.outbox(msg);
+	});
+
+	it('receive an invalid message - should send back a server error', function(done)
+	{
+		var msg = {
+			cid: "-dHkm7vtrjbWZewW6m_O",
+			args: [1, 2, 3]
+		};
+		user.on('send', function(msg)
+		{
+			assert.ok(msg.isValid);
+			assert.equal(msg.getCmd(), 'serverError');
+			assert.equal(msg.getArgs(0), 'Invalid client message');
+			done();
+		});
+		user.inbox(msg);
+	});
+
+	it('try to send an invalid meesage - should not send', function(done)
+	{
+		var msg = {
+			cid: "-dHkm7vtrjbWZewW6m_O",
+			args: [1, 2, 3]
+		};
+		user.on('send', function(msg)
+		{
+			assert.ok(msg.isValid);
+			assert.equal(msg.getCmd(), 'serverError');
+			assert.equal(msg.getArgs(0), 'Invalid client message');
 			done();
 		});
 		user.inbox(msg);
