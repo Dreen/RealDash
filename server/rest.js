ecb7ce6d7f32e6377f8e8a11641f0a2f1cfcfc0d
Dreen <dreen1@gmail.com>
2013-06-07 08:09:08 +0100

    rest api for jobs_req, needs fixing

--- server/rest.js
+++ server/rest.js
@@ -1,4 +1,5 @@
 var
+_isArray = require('underscore').isArray,
 _isObj   = require('underscore').isObject;
 
 function e404(res)
@@ -8,6 +9,14 @@ function e404(res)
 
 module.exports = function(app, db)
 {
+        // app.get('/info') // TODO
+
+
+        /*
+         * Gets Model information identified by API name.
+         * If call is specified, gets only information about specific call
+         * returns an object
+         */
         app.get('/model/:name/:call?', function(req, res)
         {
                 var model = db.collection('model');
@@ -17,10 +26,12 @@ module.exports = function(app, db)
                 {
                         if (data.length === 1 && _isObj(data[0]))
                         {
+                                // the whole model
                                 if (typeof req.params.call == "undefined")
                                 {
                                         res.status(200).json(data[0]);
                                 }
+                                // specific call
                                 else
                                 {
                                         // TODO: request a call by signature
@@ -29,16 +40,64 @@ module.exports = function(app, db)
                                         {
                                                 res.status(200).json(data[0].calls[callNum]);
                                         }
-                                        else
-                                        {
-                                                e404(res);
-                                        }
+                                        else e404(res);
                                 }
                         }
-                        else
+                        else e404(res);
+                });
+        });
+
+        /*
+         * Gets Request Jobs information identified by a call signature.
+         * start is mandatory and information is returned for the job(s) that started after or at the specified time in ms.
+         * start_end is optional, if present, returns a list of jobs that started in the specified interval. if absent, only a single job is returned.
+         * returns an object for a single job or an array for a list of jobs
+         */
+        app.get('/jobs_req/:sig/:start/:start_end?', function(req, res)
+        {
+                // input params
+                var
+                sig = req.params.sig,
+                start = parseInt(req.params.start),
+                start_end = parseInt(req.params.start_end);
+                if (!sig || isNaN(start))
+                {
+                        e404(res);
+                        return;
+                }
+
+                // query params
+                var
+                jobs_req = db.collection('jobs_req'),
+                findSpec = {
+                        'sig': sig,
+                        'start' : {$gte: start}
+                },
+                findMethod = 'findOne';
+
+                // optional query params
+                if (!isNaN(start_end))
+                {
+                        findSpec['start'] = {
+                                $and: [
+                                        {$gte: start},
+                                        {$lte: start_end}
+                                ]
+                        };
+                        findMethod = 'find';
+                }
+
+                jobs_req[findMethod](findSpec).toArray(function(err, data)
+                {
+                        if (data.length == 1 && _isObj(data[0]))
                         {
-                                e404(res);
+                                res.status(200).json(data[0]);
                         }
+                        else if (data.length > 0)
+                        {
+                                res.status(200).json(data);
+                        }
+                        else e404(res);
                 });
         });
 };
